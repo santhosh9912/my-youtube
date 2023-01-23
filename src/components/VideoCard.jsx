7369cd9d54ec6bd39edef7d08a6e44df5cb21da4
santhosh9912 <santhosh456sura@gmail.com>
2023-01-23 10:43:55 +0530

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom"; 
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-
-import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl } from "../utils/constants";
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle } from "../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
   <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
